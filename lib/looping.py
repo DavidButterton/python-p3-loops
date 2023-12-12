be2d593139d24a910770374b9aa67d876c61c41c
DavidButterton <sadhu.butterton@gmail.com>
2023-12-12 14:38:50 -0800

    submission

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while num >= 1:
+        print(num)
+        num -= 1
+        
+    print("Happy New Year!")
+        
+# print(happy_new_year())
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = [num ** 2 for num in int_list]
+    return square_list
+  
+# print(square_integers([2, 4, 6, 8]))
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        
+        
+print(fizzbuzz())
